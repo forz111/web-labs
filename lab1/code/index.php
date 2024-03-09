12c60d27779fea657ab1362b4b75167544f71b82
forz <forzes0401@gmail.com>
2024-03-10 01:38:07 +0200

    Completed task 1 in index.php

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,2 +1,11 @@
 <?php
-echo phpinfo();
+// Task 1
+$very_bad_unclear_name = "15 chicken wings";
+
+
+$order = &$very_bad_unclear_name;
+$order = "\nHello, world!";
+
+echo "\nYour order is: $very_bad_unclear_name.";
+
+echo "<br />";
