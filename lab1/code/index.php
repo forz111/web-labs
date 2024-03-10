443c76d2b3ea027d622bb6ee9061244a72e7375f
forz <forzes0401@gmail.com>
2024-03-10 02:53:03 +0200

    Task 14 completed

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -57,3 +57,158 @@ $answer -= $myNum;
 echo $answer;
 
 echo "<br />";
+
+// Task 14
+
+// Working with %
+
+$a = 10;
+$b = 3;
+
+$result = $a % $b;
+
+echo $result;
+
+echo "<br>";
+echo "\n";
+
+if ($result == 0) {
+    echo "Shared without remainder with the result: ";
+    echo $a / $b;
+}
+else {
+    echo "Divided with the remainder: ";
+    echo $result;
+}
+
+echo "<br>";
+echo "\n";
+
+// Working with degree and root
+
+$st = pow(2,10);
+echo sqrt(245);
+
+echo "<br>";
+echo "\n";
+
+$array = array(4,2,5,19,13,0,10);
+$sum = 0;
+foreach ($array as $value) {
+    $sum += pow($value, 2);
+}
+echo "The result of array operations: " . sqrt($sum);
+
+echo "<br>";
+echo "\n";
+
+// Working with rounding functions
+
+$root379 = sqrt(379);
+
+echo round($root379);
+
+echo "<br>";
+echo "\n";
+
+echo round($root379,1);
+
+echo "<br>";
+echo "\n";
+
+echo round($root379,2);
+
+echo "<br>";
+echo "\n";
+
+// floor, ceil
+
+$root587 = sqrt(587);
+
+$floorCeil = array(
+    "floor" => floor($root587),
+    "ceil" => ceil($root587)
+);
+
+// Working with min and max
+
+$minMax = array(4,-2,5,19,-130,0,10);
+
+echo min($minMax);
+
+echo "<br>";
+echo "\n";
+
+echo max($minMax);
+
+// Working with random
+
+echo rand(1,100);
+
+for ($i = 0; $i < 10; $i++) {
+    $randArr[] = rand();
+}
+
+foreach ($randArr as $value) {
+    echo $value . " ";
+}
+
+echo "<br>";
+echo "\n";
+
+// Working with the module
+
+for ($i = 0; $i < 3; $i++) {
+    $a = rand(1, 25);
+    $b = rand(1, 25);
+
+    echo abs($a - $b);
+
+    echo "<br>";
+    echo "\n";
+}
+
+$negArr = array(1,2,-1,-2,3,-3);
+$posArr = [];
+
+foreach ($negArr as $value) {
+    $posArr[] = abs($value);
+}
+
+foreach ($posArr as $value) {
+    echo $value . " ";
+}
+
+echo "<br>";
+echo "\n";
+
+// General
+
+$anyNum = 30;
+$anyArr = [];
+
+for ($i=1; $i <= 30; $i++) {
+    if (($anyNum % $i) == 0) {
+        $anyArr[] = $i;
+    }
+}
+
+foreach ($anyArr as $value) {
+    echo $value . " ";
+}
+
+echo "<br>";
+echo "\n";
+
+$sumArr = array(1,2,3,4,5,6,7,8,9,10);
+$sumMore = 0;
+$count = 0;
+
+foreach ($sumArr as $value) {
+    $sumMore += $value;
+    $count += 1;
+    if ($sumMore > 10) {
+        echo "The number of the first elements of the array: $count.";
+        break;
+    }
+}
