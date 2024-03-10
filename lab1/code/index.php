7ef6ae400e0be2c2c8b6487f1befa9f6061231c4
forz <forzes0401@gmail.com>
2024-03-10 02:03:59 +0200

    Completed task 2

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -9,3 +9,21 @@ $order = "\nHello, world!";
 echo "\nYour order is: $very_bad_unclear_name.";
 
 echo "<br />";
+
+// Task 2
+$num1 = 1;
+echo "$num1\n";
+
+$num2 = -1;
+echo "$num2\n";
+
+$num3 = 13;
+$num4 = 1;
+echo $num3 - $num4, "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo $last_month - $this_month;
+
+echo "<br />";
